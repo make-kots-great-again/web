b1116a73a2fab43a7aa5eeec17d4972f22813875
MMichotte <martin.michotte@gmail.com>
2020-10-04 18:21:44 +0200

    created component profil

--- client-side/src/app/features/features.module.ts
+++ client-side/src/app/features/features.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfilComponent } from './profil/profil.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProfilComponent],
   imports: [
     CommonModule
   ]
