8311acc1fb282c59268230b0823e03ab3cdb1737
MMichotte <martin.michotte@gmail.com>
2020-10-06 21:11:38 +0200

    linked back and front

--- client-side/src/app/core/services/user.service.ts
+++ client-side/src/app/core/services/user.service.ts
@@ -12,18 +12,4 @@ export class UserService {
     getUserById(id: string): Observable<any> {
         return this.httpClient.get<any>(`/server/api/user/${id}`);
     }
-    
-    /*
-    getUserById(id: string) {
-        this.httpClient
-            .get<any>(`/server/api/user/${id}`)
-            .subscribe(
-                (response) => {
-                    this.user = response[0];
-                },
-                (error) => {
-                    console.log('Erreur de chargement : ' + error);
-                }
-            );
-    }*/
 }
