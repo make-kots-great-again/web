14731ecd75805efb62b4bd494eb0eea66c570011
danny <morganz55057@gmail.com>
2020-10-07 19:23:31 +0200

    about to merge to master

--- client-side/src/app/features/features.module.ts
+++ client-side/src/app/features/features.module.ts
@@ -7,7 +7,6 @@ import {RegisterComponent} from './components/register/register.component';
 import {LoginComponent} from './components/login/login.component';
 import {ProfileComponent} from './components/profile/profile.component';
 
-
 const routes: Routes = [
   {path: "register", component: RegisterComponent},
   {path: "login", component: LoginComponent},
