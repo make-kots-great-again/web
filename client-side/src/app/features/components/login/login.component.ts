a0a1a6c10e350289c34a19a793adbc302954f8cd
danny <morganz55057@gmail.com>
2020-10-11 21:00:27 +0200

    updated login page

--- client-side/src/app/features/components/login/login.component.ts
+++ client-side/src/app/features/components/login/login.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  pseudo: String;
+  password: String;
+  timeLoggedIn: number;
+  isVerified: boolean;
+
+
   constructor() { }
 
   ngOnInit(): void {
