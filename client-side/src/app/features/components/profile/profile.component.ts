2966c33d3a9e684419f8e05224d71eea3b2397f2
MMichotte <martin.michotte@gmail.com>
2020-10-14 14:49:13 +0200

    added jdoc to profile componenent.ts

--- client-side/src/app/features/components/profile/profile.component.ts
+++ client-side/src/app/features/components/profile/profile.component.ts
@@ -17,7 +17,7 @@ export class ProfileComponent implements OnInit {
   editPwdForm: FormGroup;
   userProfile: User;
 
-  UserId = '4edb4ef7-2556-4807-9d2d-9eeadc4f563f'; // '79c32cb6-1f46-48bb-b914-6bab936f8cac'; //TODO récup après authentification
+  UserId = '4edb4ef7-2556-4807-9d2d-9eeadc4f563f'; // '79c32cb6-1f46-48bb-b914-6bab936f8cac'; //TODO à récupérer depuis le local storage!
 
   constructor(
     private userService: UserService,
@@ -32,6 +32,10 @@ export class ProfileComponent implements OnInit {
     });
   }
 
+  /**
+   * Fonction d'initialisation du formulaire de modification des données du profil.
+   * Déclaration et mise sous contraintes de chaque champs du formulaire
+   */
   initEditUserForm(): void {
     this.editUserForm = this.formBuilder.group({
       username: [this.userProfile.username, Validators.required],
@@ -41,11 +45,22 @@ export class ProfileComponent implements OnInit {
     });
   }
 
+  /**
+   * Ensemble de fonctions permettant de récupérer le contrôleur du champ
+   * du formulaire de modification de profil.
+   * Utilisé surtout dans le .html afin de vérifier si le champ est valide ou non.
+   *
+   * @returns : le contrôleur du champ.
+   */
   get usernameControl(): any {return this.editUserForm.get('username'); }
   get firstNameControl(): any {return this.editUserForm.get('firstName'); }
   get lastNameControl(): any {return this.editUserForm.get('lastName'); }
   get emailControl(): any {return this.editUserForm.get('email'); }
 
+  /**
+   * Fonction d'initialisation du formulaire de modification du mot de passe du profil.
+   * Déclaration et mise sous contraintes de chaque champs du formulaire
+   */
   initEditPwdForm(): void {
     this.editPwdForm = this.formBuilder.group({
       currentPwd: ['', [Validators.required]],
@@ -55,20 +70,47 @@ export class ProfileComponent implements OnInit {
     {validator: this.passwordConfirming});
   }
 
+  /**
+   * Ensemble de fonctions permettant de récupérer le contrôleur du champ
+   * du formulaire de modification de mot de passe.
+   * Utilisé surtout dans le .html afin de vérifier si le champ est valide ou non.
+   *
+   * @returns : le contrôleur du champ.
+   */
   get currentPwdControl(): any {return this.editPwdForm.get('currentPwd'); }
   get newPwdControl(): any {return this.editPwdForm.get('newPwd'); }
   get confNewPwdControl(): any {return this.editPwdForm.get('confNewPwd'); }
 
+  /**
+   * Fonction "validteur" permettant de vérifier si les mots de passes encodé dans les champs
+   * "nouveau mot de passe" et "Confirmez nouveau mot de passe" coïncident.
+   * 
+   * @param cont : le contrôleur du formulaire dont la propriété "invalid" est un boolean
+   *               OU null
+   * @returns un rien (null) si les mdp sont égaux, la propriété 'invalid' = true si les mdp ne coïncident pas.
+   */
   passwordConfirming(cont: AbstractControl): {invalid: boolean} | null {
     return cont.get('newPwd').value === cont.get('confNewPwd').value
        ? null : {invalid: true};
  }
 
+  /**
+   * Fonction "eventHandler" appelée lors d'un click sur le bouton "modifier mon profil".
+   * Permet de passer en mode "édition" du profil.
+   */
   onEditUserProfile(): void {
     this.initEditUserForm();
     this.viewMode = 'edition';
   }
 
+  /**
+   * Fonction "eventHandler" appelée lors d'un click sur le bouton "enregistrer"
+   * Cette fonction va récupérer les informations encodé dans le formulaire
+   * et va les envoyer, par le biais d'un service, à la db.
+   *
+   * Si tout ce passe bien, on repasse en mode de "consultation" du profil.
+   * Si non, un message d'erreur apparaît.
+   */
   onSaveUserProfile(): void {
     const user = {
       username: this.editUserForm.get('username').value,
@@ -86,15 +128,32 @@ export class ProfileComponent implements OnInit {
       });
   }
 
+  /**
+   * Fonction "eventHandler" appelée lors d'un click sur le bouton "annuler"
+   * Permet de repasser en mode "consultation" du profil.
+   */
   onAbort(): void {
     this.viewMode = 'view';
   }
 
+  /**
+   * Fonction "eventHandler" appelée lors d'un click sur le bouton "modifier mot de passe"
+   * Ouvre un popup et initialise son formulaire.
+   */
   onEditPwd(): void {
     this.showPopup = true;
     this.initEditPwdForm();
   }
 
+  /**
+   * Fonction "eventHandler" appelée lors d'un click sur le bouton "enregistrer"
+   * dans le popup de modification de mot de passe.
+   * Cette fonction va récupérer les informations encodé dans le formulaire
+   * et va les envoyer, par le biais d'un service, à la db.
+   *
+   * Si tout ce passe bien, le popup est fermé.
+   * Si non, un message d'erreur apparaît.
+   */
   onSaveUserPwd(): void {
     const passwords = {
       password: this.editPwdForm.get('currentPwd').value,
@@ -111,14 +170,23 @@ export class ProfileComponent implements OnInit {
       });
   }
 
+  /**
+   * Fonction "eventHandler" appelée lors de la fermeture du popup
+   * de modification du mot de passe.
+   * Permet de s'assurer que certaines variables soient ré-initialisée.
+   */
   onModalClose(): void {
     this.pwdError = false;
     this.showPopup = false;
   }
 
+  /**
+   * Fonction "eventHandler" appelée lors d'un clique dans le champ
+   * "mot de passe" dans le popup de modification du mot de passe.
+   * Permet d'effacer le contenu du champ et d'enlever les éventuels erreurs affichées.
+   */
   onPwdField(): void{
     this.pwdError = false;
     this.editPwdForm.get('currentPwd').reset();
   }
 }
-
