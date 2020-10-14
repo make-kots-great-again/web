7c32108aa4f01498f5a45e13893fb8d062151582
danny <morganz55057@gmail.com>
2020-10-14 05:17:39 +0200

    can view groups - frontend -

--- client-side/src/app/features/features.module.ts
+++ client-side/src/app/features/features.module.ts
@@ -7,16 +7,17 @@ import {SharedModule} from "../shared/shared.module";
 import {RegisterComponent} from './components/register/register.component';
 import {LoginComponent} from './components/login/login.component';
 import {ProfileComponent} from './components/profile/profile.component';
-import {AuthGuard} from '../core/guards/';
+import {AuthGuard} from "../core/guards/auth.guard";
 import {DashboardComponent} from "./components/dashboard/dashboard.component";
 import {ChartsComponent} from "./components/dashboard/charts/charts.component";
-import { GroupsComponent } from './components/groups/groups.component';
+import {GroupsComponent} from './components/groups/groups.component';
 
 const routes: Routes = [
   {path: "register", component: RegisterComponent},
   {path: "login", component: LoginComponent},
   {path: "dashboard", component: DashboardComponent, canActivate: [AuthGuard]},
   {path: "profile", component: ProfileComponent, canActivate: [AuthGuard]},
+  {path: "groups", component: GroupsComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
