7a9c883613055e57828b9a6c957d21f7e141a3cc
danny <morganz55057@gmail.com>
2020-10-16 10:01:20 +0200

    added auto backend restart

--- server-side/models/index.js
+++ server-side/models/index.js
@@ -2,7 +2,7 @@ import {Group} from "./groups";
 import {userGroup} from "./userGroups";
 import {User} from "./users";
 
-Group.belongsToMany(User, {through: userGroup, foreignKey: 'groupId', as : 'users'});
 User.belongsToMany(Group, {through: userGroup, foreignKey: 'userId', as : 'groups'});
+Group.belongsToMany(User, {through: userGroup, foreignKey: 'groupId', as : 'users'});
 
-export {Group, User, userGroup}
+export {User, Group,userGroup}
