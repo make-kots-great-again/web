95f95f2ad0a88440c52c4a24dbcb299232702afd
danny <morganz55057@gmail.com>
2020-11-01 19:34:22 +0100

    updated groups

--- client-side/src/app/features/components/groups/groups.component.ts
+++ client-side/src/app/features/components/groups/groups.component.ts
@@ -1,33 +1,23 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, Input, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, HostListener, OnInit} from '@angular/core';
 import {GroupService} from '../../../core/services/group.service';
-import {Observable, of, ReplaySubject} from 'rxjs';
-import {catchError, defaultIfEmpty, takeUntil, tap} from 'rxjs/operators';
+import {ReplaySubject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 import {AuthenticationService} from '../../../core/services/authentification.service';
-import {Group, IGroup} from '../../../shared/models/group.model';
+import {Group} from '../../../shared/models/group.model';
 
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.component.html',
-  styleUrls: ['./groups.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./groups.component.css']
 })
 export class GroupsComponent implements OnInit {
 
-  //defaultIfEmpty(false),
-
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
-  groups: Array<Group>;
+  groups: Array<Group> = [];
   filterGroups = '';
   groupName = '';
   groupId = '';
   groupDescription = '';
-
-  groupsA$: Observable<Array<Group>> = this.groupService.groups$
-    .pipe(
-      catchError(error => {
-        console.error(error);
-        return of([]);
-      }));
 
   constructor(private groupService: GroupService,
               private authenticationService: AuthenticationService,
@@ -35,10 +25,10 @@ export class GroupsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.getGroups();
+     this.getGroups();
   }
 
-  /*
+
     getGroups(): void {
     this.groupService.getMyGroups()
       .pipe(takeUntil(this.destroyed$))
@@ -50,7 +40,7 @@ export class GroupsComponent implements OnInit {
           console.log(error);
         });
   }
-   */
+
 
   ngDoCheck() {
     this.cd.markForCheck();
@@ -65,7 +55,7 @@ export class GroupsComponent implements OnInit {
     this.groupService.createGroup(groupData)
       .pipe(takeUntil(this.destroyed$))
       .subscribe(() => {
-          // this.getGroups();
+          this.getGroups();
         },
         error => {
           console.log(error);
@@ -81,7 +71,7 @@ export class GroupsComponent implements OnInit {
     this.groupService.updateGroup(this.groupId, groupData)
       .pipe(takeUntil(this.destroyed$))
       .subscribe(() => {
-          this.groupsA$.pipe(tap(() => console.log('toto')))
+          this.getGroups();
         },
         error => {
           console.log(error);
@@ -108,7 +98,7 @@ export class GroupsComponent implements OnInit {
     this.groupService.leaveGroup(this.groupId, currentUser.userId)
       .pipe(takeUntil(this.destroyed$))
       .subscribe(() => {
-          //  this.getGroups();
+           this.getGroups();
         },
         error => {
           console.log(error);
@@ -119,7 +109,7 @@ export class GroupsComponent implements OnInit {
     this.groupService.deleteGroup(this.groupId)
       .pipe(takeUntil(this.destroyed$))
       .subscribe(() => {
-          //  this.getGroups();
+            this.getGroups();
         },
         error => {
           console.log(error);
