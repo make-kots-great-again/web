13b5ad56b0d8ae85d9c77f755ace2920538c3805
diomenios <loui.arys@gmail.com>
2020-11-16 16:02:38 +0100

    add unit-tests for the products and the shoppingList

--- server-side/tests/fixtures/fakeShoppingList.js
+++ server-side/tests/fixtures/fakeShoppingList.js
@@ -1,37 +1,14 @@
+export default function makeFakeShoppingList(codeBarre, quantityOfProduct) {
+   
+    const shoppingList = {
+        code: codeBarre,
+        quantity: quantityOfProduct
+    };
 
-const partShoppingListPrototype = {
-    constructor: function (id, quantity, id_group_user, code){
-        this.id = id,
-        this.quantity = quantity,
-        this.id_group_user = id_group_user,
-        this.code = code
+    return {
+        ...shoppingList
     }
 }
-
-
-
-function makefakeList(...fakeProductsList){
-    let shoppingList = [];
-
-    for(let e in fakeProductsList){
-        let shoppingListElement = Object.create(partShoppingListPrototype);
-        shoppingListElement.constructor(e.id, Math.round(Math.random() * (8) + 1), e.id_group_user, e.code)
-        shoppingList.push(shoppingListElement);
-    }
-    
-    return shoppingList;
-};
-
-export default function makeFakeProduct (ingredient) {
-    const randomBarCode = Math.round(Math.random() * (999999999) + 100000000);
-    const randomProductName = Math.random().toString(36).substr(2, 9);
-    
-    const shoppingList = makefakeList()
-    
-    return {
-        ...product
-    }
-};
 
 /*
 
---
+++ server-side/tests/unit-tests/entities/shoppingList.js
@@ -0,0 +1,95 @@
+//During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+//bring in dev-dependencies
+import {expect} from 'chai';
+import {describe, it} from 'mocha';
+
+
+import {makeShoppingList} from '../../../domain'
+import {RequiredParameterError} from "../../../helpers/errors"
+import makeFakeShoppingList from '../../fixtures/fakeShoppingList';
+
+
+describe('SHOPPINGLIST ENTITY', () => {
+
+    describe('#shoppingList', () => {
+
+        it('it should make a shopping list', () => {
+            const shoppingList = makeFakeShoppingList(11223344, 4);
+            const buildShoppingList = makeShoppingList({...shoppingList});
+            expect(buildShoppingList.getProductCode()).to.be.eql(shoppingList.code);
+            expect(buildShoppingList.getProductQuantity()).to.be.eql(shoppingList.quantity);
+        });
+    });
+
+    describe('#code', () => {
+
+        it('a code must have a code', () => {
+            
+            const shoppingList = makeFakeShoppingList(undefined, 4);
+            
+            expect(() => makeShoppingList({...shoppingList}))
+                .to.throw(RequiredParameterError, 'A product code is a required.');
+        });
+
+        it("a code can't be null", () => {
+            
+            const shoppingList = makeFakeShoppingList(null, 4);
+            
+            expect(() => makeShoppingList({...shoppingList}))
+                .to.throw(TypeError, 'A product code must be a number.');
+        });
+
+        it("a code can't be a string", () => {
+            
+            const shoppingList = makeFakeShoppingList("404", 4);
+            
+            expect(() => makeShoppingList({...shoppingList}))
+                .to.throw(TypeError, 'A product code must be a number');
+        });
+    });
+
+    describe('#quantity', () => {
+
+        it('a quantity must have a code', () => {
+            
+            const shoppingList = makeFakeShoppingList(11223344, undefined);
+            
+            expect(() => makeShoppingList({...shoppingList}))
+                .to.throw(RequiredParameterError, 'A product quantity is a required.');
+        });
+
+        it("a quantity can't be null", () => {
+            
+            const shoppingList = makeFakeShoppingList(11223344, null);
+            
+            expect(() => makeShoppingList({...shoppingList}))
+                .to.throw(TypeError, "A product's quantity must be a number.");
+        });
+
+        it("a quantity can't be a string", () => {
+            
+            const shoppingList = makeFakeShoppingList(11223344, "404");
+            
+            expect(() => makeShoppingList({...shoppingList}))
+                .to.throw(TypeError, "A product's quantity must be a number.");
+        });
+
+        it("a quantity can't be to big", () => {
+            
+            const shoppingList = makeFakeShoppingList(11223344, 22);
+            
+            expect(() => makeShoppingList({...shoppingList}))
+                .to.throw(RangeError, "A product's quantity must be between 1 and 20.");
+        });
+
+        it("a quantity can't be small than 1", () => {
+            
+            const shoppingList = makeFakeShoppingList(11223344, 0);
+            
+            expect(() => makeShoppingList({...shoppingList}))
+                .to.throw(RangeError, "A product's quantity must be between 1 and 20.");
+        });
+    });
+});
