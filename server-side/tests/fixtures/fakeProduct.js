e4d619e014c4b3832a1ae093b6befc0927059ce0
diomenios <loui.arys@gmail.com>
2020-11-19 00:12:05 +0100

    save

--- server-side/tests/fixtures/fakeProduct.js
+++ server-side/tests/fixtures/fakeProduct.js
@@ -8,20 +8,3 @@ export default function makeFakeProduct (name) {
         ...product
     }
 }
-
-/*export default function makeFakeProduct (ingredient, product_name) {
-   
-    const randomBarCode = Math.round(Math.random() * (999999999) + 100000000);
-    const randomProductName = Math.random().toString(36).substr(2, 9);
-   
-    const product = {
-        code: randomBarCode,
-        product_name: product_name,
-        brands: randomProductName.split("").reverse().join(""),
-        ingredients: ingredient
-    };
-
-    return {
-        ...product
-    }
-}*/
